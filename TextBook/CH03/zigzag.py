cd594ec21bdf157961d8e1b4e5d831297902dc70
e23007 <e23007@std.it-college.ac.jp>
2024-05-14 01:09:59 +0000

    ジグザクP89完成

---
+++ TextBook/CH03/zigzag.py
@@ -0,0 +1,20 @@
+import time,sys,keyboard
+
+indent=0
+indent_increasing=True
+
+try:
+    while True:
+        print(' '* indent,end='')
+        print('********')
+        time.sleep(0.2)
+        if indent_increasing:
+            indent+=1
+            if indent==20:
+                indent_increasing=False
+        else:
+            indent-=1
+            if indent==0:
+                indent_increasing=True
+except KeyboardInterrupt:
+    sys.exit()
