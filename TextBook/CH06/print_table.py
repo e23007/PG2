a6975d0bbb35ad87a8154bd56068d4f2cb498f14
e23007 <e23007@std.it-college.ac.jp>
2024-05-23 00:49:45 +0000

    P189 print_table

--- TextBook/CH06/print_table.py
+++ TextBook/CH06/print_table.py
@@ -5,6 +5,10 @@ table_data = [['apples', 'oranges', 'cherries', 'banana'],
 # 最大の桁数を見つける
 max_width = len(str(max(max(row) for row in table_data)))
 
-for i in range(len(table_data[0])):
-    # 各要素を右寄せにして幅を揃える
-    print("".join(str(line[i]).rjust(max_width) for line in table_data))
+def print_table(table_data,max_width):
+    for i in range(len(table_data[0])):
+        # 各要素を右寄せにして幅を揃える
+        print("".join(str(line[i]).rjust(max_width) for line in table_data))
+
+
+print_table(table_data,max_width)
