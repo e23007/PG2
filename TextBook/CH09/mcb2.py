b893d8d28be0ed2331b351bbefa83a28ff5b0491
e23007 <e23007@std.it-college.ac.jp>
2024-05-28 03:32:25 +0000

    P280 マルチクリップボード拡張

--- TextBook/CH09/mcb2.py
+++ TextBook/CH09/mcb2.py
@@ -1,10 +1,11 @@
 #! python2
 #ターミナル(VSCodeでなく)で以下を実行
 #mcb.py -クリップボードのテキストを保存・復元
-#python3 mcb.py save <keyword> - クリップボードをキーワードに紐付けて保存
-#python3 mcb.py <keyword> - キーワードに紐づけられたテキストをクリップボードにコピー
-#python3 mcb.py list - 全キーワードをクリップボードにコピー
-#python3 mcb.py delete <keyword> - キーワードに紐づけられたテキストを削除
+#python3 mcb2.py save <keyword> - クリップボードをキーワードに紐付けて保存
+#python3 mcb2.py <keyword> - キーワードに紐づけられたテキストをクリップボードにコピー
+#python3 mcb2.py list - 全キーワードをクリップボードにコピー
+#python3 mcb2.py delete <keyword> - キーワードに紐づけられたテキストを削除
+#python3 mcb2.py clear - キーワード全削除
 import shelve,pyperclip,sys
 mcb_shelf=shelve.open('mcb')
 #TODO:クリップボードの内容を保存
@@ -20,6 +21,8 @@ elif len(sys.argv)==2:
 elif sys.argv[1].lower()=='delete':
     if sys.argv[2] in mcb_shelf:
         del mcb_shelf[sys.argv[2]]
+    elif sys.argv[2]=='clear':
+        mcb_shelf.clear()
     else:
         print('そのようなキーワードはございません')
 mcb_shelf.close()
