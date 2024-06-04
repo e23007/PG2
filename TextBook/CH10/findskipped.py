b15b8d61bd9f073462fde273fc7b11bf333b886c
e23007 <e23007@std.it-college.ac.jp>
2024-06-04 03:18:26 +0000

    P303 連番の飛びを埋める

--- TextBook/CH10/findskipped.py
+++ TextBook/CH10/findskipped.py
@@ -1,7 +1,5 @@
-import os
-import re
-import shutil
-
+# #python3 findskipped.py txt txt
+import re,os,shutil
 def find_skiped_files(folder, prefix, rename=False):
     ''' folderの中のprefixから始まるファイルの連番の飛びを調べる。
         renameがTrueなら、飛びを埋めるようにファイル名を変更する。
@@ -49,5 +47,45 @@ def find_skiped_files(folder, prefix, rename=False):
 
 # テスト用
 if __name__ == "__main__":
-    find_skiped_files('seqfiles', 'spam')
-    find_skiped_files('seqfiles', 'spam', True)
+    find_skiped_files('txt', 'txt')
+    find_skiped_files('txt', 'txt', True)
+
+
+# import os, sys, re, shutil
+
+# #ファイル名を取得
+# dir_file = sys.argv[1]
+# pre = sys.argv[2]
+# file_dict = {}
+
+# file_list = os.listdir(dir_file)
+
+# for i in range(0, len(file_list)):
+#     file_dict[file_list[i]] = pre + "{:03}.txt".format(i+1)
+
+# #index的にはあるはずなのに、ファイル名として存在しないファイルを探す
+# def find_missing_file():
+#     for fname in file_dict.values():
+#         regex = re.compile(fname)
+#         mo = regex.search(", ".join(file_dict.keys()))
+#         if not mo:
+#             print(fname)
+
+
+# #元のファイル名から、連番の番号にする
+# def change_fname(change = False):
+#     if change == True:
+#         for k, v in file_dict.items():
+#             if k != v:
+#                 print(k,v)
+#                 shutil.move(os.path.join(dir_file, k), os.path.join(dir_file, v))
+        
+# find_missing_file()
+
+# print("Do you want to change file name?:  y or [n]")
+# option = str(input())
+# if option == "y":
+#     change = True
+# else:
+#     change = False
+# change_fname(change)
