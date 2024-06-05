a4d63e912b176f7b6ddd9b04426940ebbe193d0e
e23007 <e23007@std.it-college.ac.jp>
2024-06-05 03:26:08 +0000

    P326 演習プロジェクト

---
+++ TextBook/CH11/coinFlip.py
@@ -0,0 +1,21 @@
+import random
+coin=['表','裏']
+
+def choices():
+    guess=''
+    while guess not in ('表','裏'):
+        print('コインの表裏を当ててください。表か裏を入力してください。')
+        guess=input()
+    return guess
+
+guess=choices()
+toss=coin[random.randint(0,1)] #0は裏,1は裏
+if toss==guess:
+    print('当たり!')
+else:
+    print('はずれ!もう一回当てて!')
+    guess=choices()
+    if toss==guess:
+        print('当たり!')
+    else:
+        print('はずれ。このゲームは苦手ですね。')
