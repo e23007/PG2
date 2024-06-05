122a67348303e966a6e149e6b540933732847720
e23007 <e23007@std.it-college.ac.jp>
2024-06-05 04:19:17 +0000

    P322~P323 ブレークポイント

--- TextBook/CH11/coinFlip.py
+++ TextBook/CH11/coinFlip.py
@@ -1,21 +1,9 @@
 import random
-coin=['表','裏']
 
-def choices():
-    guess=''
-    while guess not in ('表','裏'):
-        print('コインの表裏を当ててください。表か裏を入力してください。')
-        guess=input()
-    return guess
-
-guess=choices()
-toss=coin[random.randint(0,1)] #0は裏,1は裏
-if toss==guess:
-    print('当たり!')
-else:
-    print('はずれ!もう一回当てて!')
-    guess=choices()
-    if toss==guess:
-        print('当たり!')
-    else:
-        print('はずれ。このゲームは苦手ですね。')
+heads=0
+for i in range(1,1001):
+    if random.randint(0,1)==1:
+        heads=heads+1
+    if i==500:
+        print('半分完了!')
+print('表は'+str(heads)+'回出ました。')
