cd10a6ea7cd1548286e74fe6a7e3a029ecf25a56
e23007 <e23007@std.it-college.ac.jp>
2024-06-19 03:04:02 +0000

    P402 空行の挿入

---
+++ TextBook/CH13/blankRowInserter.py
@@ -0,0 +1,31 @@
+#! python3
+# python3 blankRowInserter.py 3 2 myProduce.xlsx
+
+import sys,openpyxl
+
+if len(sys.argv)<4:
+    print('使い方:python3 blankRowInserter.py <行> <挿入する行数> <変更するxlsx>')
+    sys.exit()
+
+n=int(sys.argv[1])
+m=int(sys.argv[2])
+src=sys.argv[3]
+
+wb = openpyxl.load_workbook(src)
+sheet = wb.active
+
+new_wb = openpyxl.Workbook()
+new_sheet = new_wb.active
+
+for row in range(1, sheet.max_row + 1):
+    if row < n:
+        new_row = row
+    else:
+        new_row = row + m  # m行分の空行を入れる
+
+    for col in range(1, sheet.max_column + 1):
+        old_cell = sheet.cell(column=col, row=row)
+        new_cell = new_sheet.cell(column=col, row=new_row)
+        new_cell.value = old_cell.value
+
+new_wb.save(src + '.ins.xlsx')
